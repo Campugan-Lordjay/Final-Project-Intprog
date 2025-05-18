310f5013d221d234b87da15376d520ae73c616ff
znarfieeee <watsszap23@gmail.com>
2025-05-18 23:31:56 +0800

    Yaml swagger bug fix

--- Backend/_helpers/swagger.js
+++ Backend/_helpers/swagger.js
@@ -1,10 +1,9 @@
-const express = require('express');
-const router = express.Router();
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('../backend/swagger.yaml');
+const express = require("express")
+const router = express.Router()
+const swaggerUi = require("swagger-ui-express")
+const YAML = require("yamljs")
+const swaggerDocument = YAML.load("./swagger.yaml")
 
-router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+router.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
-module.exports = router;
-
+module.exports = router
